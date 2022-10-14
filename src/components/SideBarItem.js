3b5956281931632def22d89cabed2aab2df762d6
Yinon <yinonmegi2212@gmail.com>
2022-10-14 14:42:49 +0300

    Side bar item style updated

--- src/components/SideBarItem.js
+++ src/components/SideBarItem.js
@@ -5,7 +5,7 @@ import { Outlet, Link } from "react-router-dom";
 
 const SideBarItem = (props) => {
     const pathName = window.location.pathname;
-    return <Link style={{textDecoration: 'none'}} to={props.linkTo}><Text classes={`medium sidebar-item ${pathName === `/${props.linkTo}` ? "bold" : ""}`} text={props.text} /></Link>
+    return <Link style={{textDecoration: 'none', color: "#9E9E9E"}} to={props.linkTo}><Text classes={`medium sidebar-item ${pathName === `/${props.linkTo}` ? "bold" : ""}`} text={props.text} /></Link>
 }
 
 export default SideBarItem;
